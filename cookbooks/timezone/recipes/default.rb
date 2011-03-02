47ef42c2a0686c6248dea9ac88fdca551c03fc95
rtdp <rtdp@weboniselab.com>
2011-03-02 13:18:55 +0530

    command single userd.

--- cookbooks/timezone/recipes/default.rb
+++ cookbooks/timezone/recipes/default.rb
@@ -3,7 +3,6 @@
 # Recipe:: default
 #
 execute "set-timezone-file" do
-          command "ln -sf #{File.join("/usr/share/zoneinfo/Asia/Calcutta")} /etc/localtime"
-	  command "/etc/init.d/vixie-cron restart"
+          command "ln -sf #{File.join("/usr/share/zoneinfo/Asia/Calcutta")} /etc/localtime ; /etc/init.d/vixie-cron restart"
 end
 
