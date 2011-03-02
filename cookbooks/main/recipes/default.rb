8bfda7fdccdd9e1951da8b5c23b284d6610a38a5
rtdp <rtdp@weboniselab.com>
2011-03-02 12:38:39 +0530

    Activated set time zone thing.

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -70,3 +70,5 @@
 #uncomment to include the mysql_administrative_tools recipe
 # additional configuration of this recipe is required
 #require_recipe "mysql_administrative_tools"
+
+require_recipe "timezone"
